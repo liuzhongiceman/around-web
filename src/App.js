7e5208a317891aa3c94fba8ae92a93733d237b7e
liuzhongiceman <zhongliu884@gmail.com>
2019-06-30 15:11:00 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import {TopBar} from './TopBar';
+import {Register} from './Register';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+        <TopBar/>
+        <Register/>
+    </div>
+  );
+}
+
+export default App;
